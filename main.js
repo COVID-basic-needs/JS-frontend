769bb4237146f80e971bad99df233c836b5dea40
Roshan <roshan.palakkal@gmail.com>
2020-03-28 20:02:26 -0400

    added algolia

--- main.js
+++ main.js
@@ -9,11 +9,28 @@ var map = new google.maps.Map(document.getElementById("map"), {
 
 var geocoder = new google.maps.Geocoder();
 
+const client = algoliasearch('JWHPBFC4T1', '6eb371014c3bff23b98dde01a8ef1763');
+const index = client.initIndex('us_foodbank');
+
+function findResults(lat, lng){
+
+    index.search('', {
+
+        aroundLatLng: lat + ", " + lng,
+        aroundRadius: 5000
+      }).then(({ hits }) => {
+        console.log(hits)
+        hits.map(createMarker)
+      });
+}
+
+
+
 
 //to center the map
 function codeAddress() {
-
     var address = document.getElementById("zipCode").value;
+
 
     geocoder.geocode({
         'address': address,
@@ -21,17 +38,29 @@ function codeAddress() {
             'country': 'US'
         }
     }, function (results, status) {
+        var codedLat
+        var codedLng
 
         if (status == google.maps.GeocoderStatus.OK) {
             map.setCenter(results[0].geometry.location);
+            map.setZoom(12);
+            codedLat = results[0].geometry.location.lat();
+            codedLng = results[0].geometry.location.lng();
+            findResults(codedLat, codedLng);
         } else {
             alert("Geocode was not successful for the following reason: " + status);
         }
     });
+    
 }
 
 
-function createMarker(name, add,lat,lng) {
+function createMarker(hit) {
+    console.log(hit)
+    var name = hit["siteName"]
+    var add = hit["siteAddress"]
+    var lat = hit["_geoloc"]["lat"]
+    var lng = hit["_geoloc"]["lng"]
 
     var contentString = "Name " + name +'<br>' +
     "Address: " + add + '<br>'; //+
@@ -49,5 +78,4 @@ function createMarker(name, add,lat,lng) {
 
 }
 
-createMarker("John Doe High School", "9351 Washington St, Thornton, CO 80229", 39.8664037, -104.9805197)
 
