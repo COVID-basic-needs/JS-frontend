3e59a332a752fdb8b7999791dc461ccb455f5ca9
rpalakkal <roshan.palakkal@gmail.com>
2020-03-30 00:51:23 -0400

    tried to fix popover for gh page

--- main.js
+++ main.js
@@ -170,21 +170,16 @@ $( document ).ready(function() {
 });
 
 
-$('body')
-  .on('mousedown', '.popover', function(e) {
-    e.preventDefault()
-});
+// $('body')
+//   .on('mousedown', '.popover', function(e) {
+//     e.preventDefault()
+// });
 
 
-var about = `<p>Darcie is an automated phone line anyone can call to find human services near them, such as free food, legal assistance, non-emergency medical help, and more.
-Read more and watch a live stream of the conversations at <a href="http://www.darcie.me">darcie.me</a>  <br/>     
-<b>COVID-19 Update</b>            
-Darcie was intended to pull from all services listed in the <a href="https://sfserviceguide.org/">SF Service Guide</a>, however in the current times the format of the data in that database (a.k.a. <a href="https://github.com/sheltertechsf/askdarcel-api">AskDarcel on github</a>) made it hard to keep the information up to date with service hours & offerings changing.
-We pivoted Darcie to pull from a seperate Algolia index which consists of all hygiene stations & places handing out food in SF. The dialog & webhook have been adopted accordingly.</p>`
+var about = `<p>Darcie is an automated phone line anyone can call to find human services near them, such as free food, legal assistance, non-emergency medical help, and more. Read more and watch a live stream of the conversations at <a href="http://www.darcie.me">darcie.me</a> <br/> <b>COVID-19 Update</b> Darcie was intended to pull from all services listed in the <a href="https://sfserviceguide.org/">SF Service Guide</a>, however in the current times the format of the data in that database (a.k.a. <a href="https://github.com/sheltertechsf/askdarcel-api">AskDarcel on github</a>) made it hard to keep the information up to date with service hours & offerings changing. We pivoted Darcie to pull from a seperate Algolia index which consists of all hygiene stations & places handing out food in SF. The dialog & webhook have been adopted accordingly.</p>`
 $('#about').popover({
     content: about,
-    trigger: 'focus',
-    placement:'bottom'
+    trigger: 'focus'
 
 })
 
@@ -195,8 +190,7 @@ While we actively accept help, as well as encourage you to fork this repo and bu
 
 $('#contact').popover({
     content: contactInfo,
-    trigger: 'focus',
-    placement:'bottom'
+    trigger: 'focus'
 
 })
 
